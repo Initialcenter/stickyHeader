69ab0b641e9807072d311c8bab11d9e2cac4109c
用户01 <7675929+yuwenkai1@user.noreply.gitee.com>
2025-05-07 09:46:46 +0800

    style: 代码格式化

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,7 +8,7 @@ const routes: Array<RouteConfig> = [
   {
     path: "/",
     name: "home",
-    component: HomeView,
+    component: () => import("../views/test.vue"),
   },
   {
     path: "/test",
